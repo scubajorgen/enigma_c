0446754ac87873dc7ead96e7c865372391f8b9a8
jorgen <scubajorgen@gmail.com>
2019-03-17 14:07:03 +0100

    Added example to main loop

--- src/main.c
+++ src/main.c
@@ -14,9 +14,32 @@ int main()
     printf("ENIGMA SIMULATION\n");
     printf("###############################################################\n");
     
+    char  encoded[]="vztuq ordcb sirgg pnwrl lwxvp";
+    char* decoded;
         
-  
-    message06();
+    Enigma* enigma;
+    
+    enigma=createEnigmaM3();
+    
+    placeWaltze         (enigma, 1, "I");
+    placeWaltze         (enigma, 2, "II");
+    placeWaltze         (enigma, 3, "III");
+    setRingStellungen   (enigma, "26 13 01");
+    setGrundStellungen  (enigma, "01 02 03");
+    
+    placeSteckers       (enigma, "bq cr di ej kw mt os px uz gh");
+    placeUmkehrWaltze   (enigma, "UKW B");
+    
+    setText             (enigma, encoded);
+    
+    encodeDecode(enigma);
+    
+    decoded=toString(enigma);
+    
+    printf("Encoded: %s \nDecoded %s\n", encoded, decoded);
+    
+    destroyEnigma(enigma);
+   
     
     return 0;
 }
